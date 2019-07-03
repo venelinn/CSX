3fee9f619be391b9609f29fa1c16d4a883824825
Venelin Nikolov <venelin.nikolov@cossette.com>
2019-07-03 12:43:28 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
+<p align="center">
+  <a href="https://venelin.ca">
+    <img alt="Venelin.ca" src="src/images/vca-icon.png" />
+  </a>
+</p>
+<h1 align="center">
+  Venelin.ca with GatsbyJs
+</h1>
+
---
+++ src/components/About/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Profile from './profile';
+import Fade from 'react-reveal/Fade'
+//import CTA from './cta';
+
+import './about.scss';
+
+const About = props => {
+  return (
+    <>
+      <Fade delay={500}>
+      <div
+        className='about__intro'
+        dangerouslySetInnerHTML={{
+          __html: props.about.content.childContentfulRichText.html
+        }}
+      />
+      <div className='about__content'>
+        <Profile info={props.about.modules[0]} />
+      </div>
+      </Fade>
+      {/* <CTA /> */}
+    </>
+  );
+};
+
+export default About;
