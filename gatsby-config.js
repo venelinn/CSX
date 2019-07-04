c74e15786034c45fb25f01c7fea68c4f16e018ab
Venelin Nikolov <venelinn@gmail.com>
2019-07-03 21:11:08 -0400

    Setup Contentful and Cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,13 +50,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `data`,
-        path: `${__dirname}/src/data`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `src`,
         path: `${__dirname}/src`,
       },
